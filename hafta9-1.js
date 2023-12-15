022f851c47dc23a52529582f44ce1e3148611a04
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2023-12-15 11:07:25 +0300

    Update hafta9-1.js

--- hafta9-1.js
+++ hafta9-1.js
@@ -1,6 +1,15 @@
-// Resimlerin adlarını değiştirerek kendi resim dosyalarınızı ekleyebilirsiniz.
 const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
 let currentIndex = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Sayfa yüklendiğinde ve her yenilendiğinde arka plan rengini değiştir
+  changeBackgroundColor();
+});
+
+function changeBackgroundColor() {
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+}
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -11,17 +20,11 @@ function getRandomColor() {
   return color;
 }
 
-function toggleImage1() {
-  const imageElements = document.querySelectorAll('img');
-  imageElements.forEach(img => {
-    img.style.display = 'none';
-  });
+function toggleImage(index) {
+  const selectedImageIndex = index - 1;
+  const selectedImage = document.getElementById(`image${index}`);
+  const displayStyle = selectedImage.style.display;
 
-  currentIndex = (currentIndex + 1) % images.length;
-  const currentImage = document.getElementById(`image${currentIndex}`);
-  currentImage.style.display = 'block';
+  // Resmi göster veya gizle
+  selectedImage.style.display = displayStyle === 'none' || displayStyle === '' ? 'block' : 'none';
 }
-
-
-
-
